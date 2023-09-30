026525c32a529e6a284437e41d119b1df01df078
Aakansha08 <08.aakansha@gmail.com>
2023-09-30 08:47:33 -0400

    Made some changes in both the java files. the get command with and without verbose is working fine now.

--- src/main/java/HttpClient.java
+++ src/main/java/HttpClient.java
@@ -1,9 +1,102 @@
+//import java.io.*;
+//import java.net.*;
+//import java.nio.charset.StandardCharsets;
+//
+//public class HttpClient {
+//
+//    public static void httpGET(String url, int port) throws IOException, URISyntaxException {
+//
+//        URI uri = new URI(url);
+//        String host = uri.getHost();
+//        String path = uri.getRawPath();
+//        String queryParameters = uri.getRawQuery();
+//        Socket socket = new Socket(host, port);
+//
+//        //request line
+//        PrintWriter out = new PrintWriter(socket.getOutputStream());
+//        out.println(String.format("GET %s?%s HTTP/1.0",path,queryParameters));
+//        out.println("Host:" + host);
+//        out.println("User-Agent:" + "Concordia-HTTP/1.0");
+//        out.println("Accept:"+ "application/json");
+//        out.println("");
+//        out.flush();
+//
+//        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+//
+//        String responseLine = in.readLine();
+//        System.out.println(responseLine);
+//        String line;
+//
+//        //read the response headers
+//        while (!(line = in.readLine()).isEmpty()) {
+//            System.out.println(line);
+//        }
+//
+//        // Read the response body from the server, if any
+//        while ((line = in.readLine()) != null) {
+//            System.out.println(line);
+//        }
+//
+//        in.close();
+//        out.close();
+//        socket.close();
+//    }
+//
+//    public static void httpPOST(String URL, String parameters, String headers, int port) throws IOException, URISyntaxException
+//    {
+//        URI uri = new URI(URL);
+//        String host = uri.getHost();
+//        System.out.println(host);
+//        String path = uri.getRawPath();
+//        Socket socket = new Socket(host, port);
+//        PrintWriter out = new PrintWriter(socket.getOutputStream());
+//
+//        //request line
+//        out.println(String.format("POST %s HTTP/1.0",path));
+//        out.println("Host: " + host);
+//        out.println(headers);
+//        out.println("Content-Length: " + parameters.toString().length());
+//        out.println("User-agent: " + "Concordia-HTTP/1.0");
+//        out.println();
+//        out.println(parameters.toString());
+//        out.flush();
+//
+//        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+//
+//
+//        String responseLine = in.readLine();
+//        System.out.println(responseLine);
+//        String line;
+//
+//        // Read the response headers
+//        while(!(line =in.readLine()).isEmpty())
+//        {
+//            System.out.println(line);
+//        }
+//
+//        // Read the response body
+//        while((line =in.readLine())!=null)
+//        {
+//            System.out.println(line);
+//        }
+//        in.close();
+//        out.close();
+//        socket.close();
+//    }
+//
+//    public static void main(String[] args) throws IOException, URISyntaxException {
+//        httpGET("http://httpbin.org/get?course=networking&assignment=1",80);
+//        httpPOST("http://httpbin.org/post","{\"Assignment\": 1,\"Course}","Content-Type: application/json", 80);
+//    }
+//}
+
 import java.io.*;
 import java.net.*;
+import java.nio.charset.StandardCharsets;
 
 public class HttpClient {
 
-    public static void httpGET(String url, int port) throws IOException, URISyntaxException {
+    public static void httpGET(String url, int port, boolean verbose) throws IOException, URISyntaxException {
 
         URI uri = new URI(url);
         String host = uri.getHost();
@@ -13,36 +106,43 @@ public class HttpClient {
 
         //request line
         PrintWriter out = new PrintWriter(socket.getOutputStream());
-        out.println(String.format("GET %s?%s HTTP/1.0",path,queryParameters));
+        out.println(String.format("GET %s?%s HTTP/1.0", path, queryParameters));
         out.println("Host:" + host);
         out.println("User-Agent:" + "Concordia-HTTP/1.0");
-        out.println("Accept:"+ "application/json");
+        out.println("Accept:" + "application/json");
         out.println("");
         out.flush();
 
         BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
         String responseLine = in.readLine();
-        System.out.println(responseLine);
+        if (verbose) {
+            System.out.println(responseLine);
+        }
         String line;
 
         //read the response headers
         while (!(line = in.readLine()).isEmpty()) {
-            System.out.println(line);
+            if (verbose) {
+                System.out.println(line);
+            }
         }
 
         // Read the response body from the server, if any
+        StringBuilder responseBody = new StringBuilder();
         while ((line = in.readLine()) != null) {
-            System.out.println(line);
+            responseBody.append(line).append("\n");
         }
 
         in.close();
         out.close();
         socket.close();
+
+        // Print the response body
+        System.out.println(responseBody.toString());
     }
 
-    public static void httpPOST(String URL, String parameters, String headers, int port) throws IOException, URISyntaxException
-    {
+    public static void httpPOST(String URL, String parameters, String headers, int port, boolean verbose) throws IOException, URISyntaxException {
         URI uri = new URI(URL);
         String host = uri.getHost();
         System.out.println(host);
@@ -51,40 +151,65 @@ public class HttpClient {
         PrintWriter out = new PrintWriter(socket.getOutputStream());
 
         //request line
-        out.println(String.format("POST %s HTTP/1.0",path));
+        out.println(String.format("POST %s HTTP/1.0", path));
         out.println("Host: " + host);
         out.println(headers);
         out.println("Content-Length: " + parameters.toString().length());
         out.println("User-agent: " + "Concordia-HTTP/1.0");
-        out.println();
+        out.println("");
         out.println(parameters.toString());
         out.flush();
 
         BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
-
         String responseLine = in.readLine();
-        System.out.println(responseLine);
+        if (verbose) {
+            System.out.println(responseLine);
+        }
         String line;
 
         // Read the response headers
-        while(!(line =in.readLine()).isEmpty())
-        {
-            System.out.println(line);
+        while (!(line = in.readLine()).isEmpty()) {
+            if (verbose) {
+                System.out.println(line);
+            }
         }
 
         // Read the response body
-        while((line =in.readLine())!=null)
-        {
-            System.out.println(line);
+        StringBuilder responseBody = new StringBuilder();
+        while ((line = in.readLine()) != null) {
+            responseBody.append(line).append("\n");
         }
+
         in.close();
         out.close();
         socket.close();
+
+        // Print the response body
+        System.out.println(responseBody.toString());
     }
 
     public static void main(String[] args) throws IOException, URISyntaxException {
-        httpGET("http://httpbin.org/get?course=networking&assignment=1",80);
-        httpPOST("http://httpbin.org/post","{\"Assignment\": 1,\"Course}","Content-Type: application/json", 80);
+        String url = "http://httpbin.org/get?course=networking&assignment=1";
+        int port = 80;
+        boolean verbose = false;
+
+        // Check if verbose flag (-v) is provided
+        for (String arg : args) {
+            if (arg.equals("-v")) {
+                verbose = true;
+                break;
+            }
+        }
+
+        // Send the GET request with or without verbose output
+        httpGET(url, port, verbose);
+
+        String postUrl = "http://httpbin.org/post";
+        String postParameters = "{\"Assignment\": 1,\"Course\": \"networking\"}";
+        String postHeaders = "Content-Type: application/json";
+
+        // Send the POST request with or without verbose output
+        httpPOST(postUrl, postParameters, postHeaders, port, verbose);
     }
 }
--- src/main/java/httpc.java
+++ src/main/java/httpc.java
@@ -1,30 +1,121 @@
-import joptsimple.OptionParser;
-import joptsimple.OptionSet;
+////import joptsimple.OptionParser;
+////import joptsimple.OptionSet;
+////
+////import static java.util.Arrays.asList;
+////
+////public class httpc {
+////    public static void runClient()
+////    {
+////        System.out.println("Hi");
+////    }
+////
+////    public static void main(String[] args){
+////        OptionParser parser = new OptionParser();
+////        parser.acceptsAll(asList("host", "h"),"Hostname")
+////                .withOptionalArg()
+////                .defaultsTo("localhost");
+////
+////        parser.acceptsAll(asList("port", "p"), "port number")
+////                .withOptionalArg()
+////                .defaultsTo("80");
+////
+////        OptionSet opts = parser.parse(args);
+////
+////        String host = (String) opts.valueOf("host");
+////        int port = Integer.parseInt((String)opts.valueOf("port"));
+////        System.out.println(host);
+////        System.out.println(port);
+////        runClient();
+////    }
+////}
 
-import static java.util.Arrays.asList;
+import java.io.IOException;
+import java.net.URISyntaxException;
 
 public class httpc {
-    public static void runClinet()
-    {
-        System.out.println("Hi");
-    }
 
-    public static void main(String[] args){
-        OptionParser parser = new OptionParser();
-        parser.acceptsAll(asList("host", "h"),"Hostname")
-                .withOptionalArg()
-                .defaultsTo("localhost");
+    public static void main(String[] args) {
+        if (args.length < 2) {
+            System.out.println("Usage: httpc (get|post) [-v] (-h 'k:v')* [-d inline-data] [-f file] URL");
+            return;
+        }
 
-        parser.acceptsAll(asList("port", "p"), "port number")
-                .withOptionalArg()
-                .defaultsTo("80");
+        String method = args[0];
+        boolean verbose = false;
+        String inlineData = null;
+        String fileData = null;
+        String url = null;
+        String headers = "";
 
-        OptionSet opts = parser.parse(args);
+        // Parse command-line arguments
+        for (int i = 1; i < args.length; i++) {
+            String arg = args[i];
+            switch (arg) {
+                case "-v":
+                    verbose = true;
+                    break;
+                case "-d":
+                    i++;
+                    if (i < args.length) {
+                        inlineData = args[i];
+                    }
+                    break;
+                case "-f":
+                    i++;
+                    if (i < args.length) {
+                        fileData = args[i];
+                    }
+                    break;
+                case "-h":
+                    i++;
+                    if (i < args.length) {
+                        headers += args[i] + "\r\n";
+                    }
+                    break;
+                default:
+                    url = arg;
+                    break;
+            }
+        }
 
-        String host = (String) opts.valueOf("host");
-        int port = Integer.parseInt((String)opts.valueOf("port"));
-        System.out.println(host);
-        System.out.println(port);
-        runClinet();
+        if (method.equals("get") && (inlineData != null || fileData != null)) {
+            System.out.println("GET request should not have -d or -f options.");
+            return;
+        }
+
+        if (method.equals("post") && inlineData != null && fileData != null) {
+            System.out.println("POST request should have either -d or -f, but not both.");
+            return;
+        }
+
+        if (url == null) {
+            System.out.println("URL is required.");
+            return;
+        }
+
+        try {
+            HttpClient httpClient = new HttpClient();
+
+            if (method.equals("get")) {
+                if (verbose) {
+                    System.out.println("HTTP GET Request:");
+                    System.out.println("URL: " + url);
+                    System.out.println("Headers: " + headers);
+                    System.out.println();
+                }
+                httpClient.httpGET(url, 80, verbose);
+            } else if (method.equals("post")) {
+                if (verbose) {
+                    System.out.println("HTTP POST Request:");
+                    System.out.println("URL: " + url);
+                    System.out.println("Headers: " + headers);
+                    System.out.println("Data: " + (inlineData != null ? inlineData : fileData));
+                    System.out.println();
+                }
+                httpClient.httpPOST(url, (inlineData != null ? inlineData : fileData), headers, 80, verbose);
+            }
+        } catch (IOException | URISyntaxException e) {
+            e.printStackTrace();
+        }
     }
 }
