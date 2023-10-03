599a84e0995fbec595678c06376bc794aca0d180
xsrujanax <srujana.guttula@outlook.com>
2023-10-02 21:19:22 -0400

    Edited messages format

--- src/main/java/Messages.java
+++ src/main/java/Messages.java
@@ -1,36 +1,30 @@
 public class Messages {
     public static void printHelpMessage(){
-        System.out.println("""
-                httpc is a curl-like application but supports HTTP protocol only.
-                Usage:
-                 httpc command [arguments]
-                The commands are:
-                 get executes a HTTP GET request and prints the response.
-                 post executes a HTTP POST request and prints the response.
-                 help prints this screen.
-                Use "httpc help [command]" for more information about a command""");
+        System.out.println("httpc is a curl-like application but supports HTTP protocol only.\n" +
+                "Usage:\n" +
+                " httpc command [arguments]\n" +
+                "The commands are:\n" +
+                " get executes a HTTP GET request and prints the response.\n" +
+                " post executes a HTTP POST request and prints the response.\n" +
+                " help prints this screen.\n" +
+                "Use \"httpc help [command]\" for more information about a command.\n");
     }
 
     public static void printHelpGETMessage(){
-        System.out.println("""
-                httpc help get
-                usage: httpc get [-v] [-h key:value] URL
-                Get executes a HTTP GET request for a given URL.
-                 -v Prints the detail of the response such as protocol, status,\s
-                and headers.
-                 -h key:value Associates headers to HTTP Request with the format\s
-                'key:value'.""");
+        System.out.println("usage: httpc get [-v] [-h key:value] URL\n" +
+                "Get executes a HTTP GET request for a given URL.\n" +
+                " -v Prints the detail of the response such as protocol, status, and headers.\n" +
+                " -h key:value Associates headers to HTTP Request with the format 'key:value'.");
     }
 
     public static void printHelpPOSTMessage() {
-        System.out.println("""
-                httpc help post
-                usage: httpc post [-v] [-h key:value] [-d inline-data] [-f file] URL
-                Post executes a HTTP POST request for a given URL with inline data or from file.
-                 -v Prints the detail of the response such as protocol, status, and headers.
-                 -h key:value Associates headers to HTTP Request with the format 'key:value'.
-                 -d string Associates an inline data to the body HTTP POST request.
-                 -f file Associates the content of a file to the body HTTP POST request.
-                Either [-d] or [-f] can be used but not both.""");
+        System.out.println("usage: httpc post [-v] [-h key:value] [-d inline-data] [-f file] URL\n" +
+                "Post executes a HTTP POST request for a given URL with inline data or from \n" +
+                "file.\n" +
+                " -v Prints the detail of the response such as protocol, status, and headers.\n" +
+                " -h key:value Associates headers to HTTP Request with the format 'key:value'.\n" +
+                " -d string Associates an inline data to the body HTTP POST request.\n" +
+                " -f file Associates the content of a file to the body HTTP POST request.\n" +
+                "Either [-d] or [-f] can be used but not both.");
     }
 }
